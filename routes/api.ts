4e3e59b77ba098ab74566c1ccbc216dea9854f8e
shuhasegawa <shuhasegawa0430@gmail.com>
2023-04-23 17:25:39 +0900

    CORSの設定

--- routes/api.ts
+++ routes/api.ts
@@ -1,5 +1,22 @@
 import express from 'express'
 export const router = express.Router()
+
+// アクセスを許可するオリジン
+const allowList = ['http://localhost:3000', 'http://site.example:3000']
+// use()リクエスト送信時に必ず実行
+router.use((req, res, next) => {
+  // リクエスト元のオリジンが許可リストに含まれている場合、オリジンを許可
+  if (req.headers.origin && allowList.includes(req.headers.origin)) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+  }
+  // プリフライトリクエストの送信時にX-Tokenを許可する
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', 'X-Token')
+  }
+  // これがないと処理がここで終わる。（router.getやpostなどの関数に行かない）
+  next()
+})
+
 router.get('/', (req, res) => {
   // headerの設定
   res.setHeader('X-timestamp', Date.now())
@@ -10,7 +27,7 @@ router.get('/', (req, res) => {
   // responseの送信
   const param = req.query.message
   if (!param) {
-    res.status(400)
+    //res.status(400)
   }
   res.send({ message })
 })
